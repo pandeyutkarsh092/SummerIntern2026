a271ce88842bd29b5eeb576394408007e854f0cd
pandeyutkarsh092 <pandeyutkarsh092@gmail.com>
2026-06-18 11:23:26 +0530

    aaj maina padha

---
+++ HTML/FD10/code.js
@@ -0,0 +1,9 @@
+a=9
+b=3,c=10
+//console.log(a+b+c);
+
+
+// + (subtraction)
+a=9
+b=3
+console.log(a-b);
---
+++ HTML/homework.js
@@ -0,0 +1,11 @@
+let name ="Utkarsh"
+console.log(name);
+let age =21
+console.log(age);
+let college ="RITM"
+console.log(college);
+let a=50
+a=100
+console.log(a);
+const country = "INDIA"
+console.log(country);
