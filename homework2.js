a81f84802c61f91a2717f33b738c84ada4cc8e02
pandeyutkarsh092 <pandeyutkarsh092@gmail.com>
2026-06-19 06:38:10 +0530

    homework of 18 june

---
+++ homework2.js
@@ -0,0 +1,15 @@
+let name ="Utkarsh"
+console.log(name);
+console.log(typeof(name));
+let age = 21
+console.log(age);
+console.log(typeof(age));
+let height = 124
+console.log(height);
+console.log(typeof(height));
+let percentagemarks = 80.69
+console.log(percentagemarks);
+console.log(typeof(percentagemarks));
+let intern = "true"
+console.log(intern);
+console.log(typeof(intern));
