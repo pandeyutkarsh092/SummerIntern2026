25b4cc5f30e95d8b8871a33c31f902c206e64a90
pandeyutkarsh092 <pandeyutkarsh092@gmail.com>
2026-06-19 06:54:59 +0530

    HW

---
+++ homework3.js
@@ -0,0 +1,11 @@
+english = 45
+maths =30
+hindi=48
+science = 41
+sst = 40
+total =  (english + maths + hindi + science + sst );
+console.log(total);
+average = (total/5);
+console.log(average);
+percentage = (total/250)*100;
+console.log(percentage);
