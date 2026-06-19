64f5006b29b3a6dc2601c6909f87e3985258b06c
pandeyutkarsh092 <pandeyutkarsh092@gmail.com>
2026-06-19 11:01:17 +0530

    comparision op

---
+++ FD11/COMPARISIONOP.js
@@ -0,0 +1,29 @@
+//a=5
+//b=5
+//console.log(a==b);
+
+//a=5
+//b='5'
+//console.log(a==b);
+//console.log(a===b)
+
+//a=3
+//b=3.0
+//console.log(a==b);
+//console.log(a===b)
+
+//a=2
+//b='2'
+//console.log(a==b);
+//console.log(a===b)
+
+//Logical Operator
+a=5
+b=8
+console.log((a>b && b<a));
+console.log((a<b && b<a));
+console.log((a>b && b>a));
+
+
+
+
