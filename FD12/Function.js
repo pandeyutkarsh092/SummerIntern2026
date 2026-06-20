1bcc989b8f636652ab8e2041f2072fbe1bbae8fc
pandeyutkarsh092 <pandeyutkarsh092@gmail.com>
2026-06-20 11:03:21 +0530

    ajj loop aur function padha

---
+++ FD12/Function.js
@@ -0,0 +1,15 @@
+// Fuction without argument
+
+// function harsh(){
+//     console.log("Mera naam harsh hai !");
+// }
+
+// harsh()
+// harsh()
+// harsh()
+
+function multiplication(a,b,c){
+    console.log("Multiplication of numbers :", a*b*c);
+}
+
+multiplication(2,4,6)
---
+++ FD12/Loops.js
@@ -0,0 +1,19 @@
+// for(let i=1;i<=10;i++){
+//     console.log("Hello world");
+// console.log(i)
+// }
+
+//write a program to print table of 2
+
+
+// for(i=2;i<=20;i+=2){
+//  console.log(i);
+
+// }
+
+sum=0
+for(i=1;i<=5;i++)
+{
+ sum=sum+i   
+}
+console.log(sum);
